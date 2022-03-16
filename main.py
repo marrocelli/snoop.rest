fac654004edc5e366a0c534a028bebeaec52c97f
mpm <matthewmarrocelli@outlook.com>
2022-03-16 12:34:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, render_template, request, url_for
+from flask_bootstrap import Bootstrap
+import random
+import json
+
+app = Flask(__name__)
+Bootstrap(app)
+
+with open('quotes.json', 'r') as f:
+    all_quotes = json.load(f)
+
+
+def get_quote():
+    quote = random.choice(all_quotes)
+    return quote
+
+
+@app.route('/')
+def home():
+    quote = get_quote()
+    return render_template('index.html', get_quote=get_quote, quote=quote, all_quotes=all_quotes)
+
+
+@app.route('/get')
+def api_GET():
+    quote = get_quote().split('"')[1]
+    return jsonify(quote=quote)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
